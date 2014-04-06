6daedb5fe21c511b58c9baf060315c569e307385
BJ Neilsen <bj.neilsen@gmail.com>
2014-04-06 09:50:52 -0600

    Add lien project for clojure

---
+++ clojure/seven-langs/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to seven-langs
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ clojure/seven-langs/test/seven_langs/core_test.clj
@@ -0,0 +1,7 @@
+(ns seven-langs.core-test
+  (:require [clojure.test :refer :all]
+            [seven-langs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
